121435cd18668956c48e891803f49afce43696f6
vaishnavikhopale <vskhopale7@gmail.com>
2023-02-08 12:18:54 +0530

    add

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ LOGISTIC_MODEL_FILE_PATH = r"artifacts/regression_mode.pkl"
 
 KNN_MODEL_FILE_PATH = r"artifacts/knn_model.pkl"
 
-LABEL_MODEL_FILE_PATH = r"artifacts/labelencoder_y.pkl"
+# LABEL_MODEL_FILE_PATH = r"artifacts/labelencoder_y.pkl"
 
 STD_MODEL_FILE_PATH = r"artifacts/std_scaler.pkl"
 
